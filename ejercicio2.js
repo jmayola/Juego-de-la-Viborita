7e0c99852c91db49d2eece765cf0097044fddf1f
mayola <mayola@marketpsychdata.com>
2024-05-30 15:48:59 -0300

    añadidos estilos, añadido funcion de movimiento

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,23 +1,27 @@
 let s;
 let scl = 20;
 let food;
-let posxS = 10
-let posyS = 10
+let flag = 0
+let posxS = 100
+let posyS = 100
+let width = 700
+let height = 700
 function setup() {
-  createCanvas(700, 700);
+  createCanvas(width, height);
   s = new Snake();
   frameRate(10); 
-	food = createVector(random(width), random(height));
-	pickLocation(); 
+	//food = createVector(random(width), random(height));
+	//pickLocation(); 
 }
 class Snake {
-   Snake(){
+  Show(){
     fill(0,255,0)
     rect(posxS, posyS, scl, scl)
   }
-    eat() {
-      if()
-    }
+  Dir(x,y){
+        posxS +=x
+        posyS +=y
+  }
 }
 //function to store snake's location on the grid
 //floor calculates the closest int value that is less than or equal to the value of the parameter.
@@ -26,36 +30,39 @@ function pickLocation() {
 	var rows = floor(height/scl);
   food = createVector(floor(random(cols)), floor(random(rows)));//this ensure the food is in the grid aligned with snake
 	food.mult(scl);//to expand it back out
-
-
 }
 
 function draw() {
   background(51);
-  
+
   //if snake eat food, pick location
-  if (s.eat(food)) {
-  	pickLocation();
-  }
-  s.death();
-  s.update();
-  s.show();
-	
+  //if (s.eat(food)) {
+  //	pickLocation();
+  //}
+  //s.death();
+  //s.update();
   //drawing snake food
-  fill(255, 0, 100);
-  rect(food.x, food.y, scl, scl);
-  
+  //fill(255, 0, 100);
+  //rect(food.x, food.y, scl, scl);
+  s.Show();
 }
 
-function keyPressed() {
-  if (keyCode === UP_ARROW) {
-    s.dir(0, -1); //moves 0 along x and -1 (up) along y axis
-  } else if (keyCode === DOWN_ARROW) {
-    s.dir(0, 1);
-  } else if (keyCode === RIGHT_ARROW) {
-    s.dir(1, 0);
-  } else if (keyCode === LEFT_ARROW) {
-    s.dir(-1, 0);
+function keyPressed(keyCode) {
+  let s = new Snake()
+  console.log(keyCode)
+  switch(keyCode.key){
+    case 'ArrowUp':
+    s.Dir(0, -scl); //moves 0 along x and -1 (up) along y axis
+    break
+    case 'ArrowDown':
+    s.Dir(0, scl); //moves 0 along x and -1 (up) along y axis
+    break
+    case 'ArrowLeft':
+    s.Dir(-scl, 0); //moves 0 along x and -1 (up) along y axis
+    break
+    case 'ArrowRight':
+    s.Dir(scl, 0); //moves 0 along x and -1 (up) along y axis
+    break
   }
 }
 
