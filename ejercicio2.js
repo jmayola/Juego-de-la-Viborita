5b9055a297decfc9a42309145ed114524d0cfe6e
abigc23 <rocioguticao@gmail.com>
2024-05-30 14:28:09 -0300

    safe commit

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,39 +1,57 @@
-let rectX;
-let rectY;
-let ellX = 10;
-let ellY = 10;
-let nz = 5;
-let xz = 5;
+// 1. Press Play at the top
+// 2. Click on dark grey canvas (before snake hit the wall)
+// 3. Use key arrows to move snake
+
+var s;
+var scl = 20;
+var food;
+
 function setup() {
-  createCanvas(700,600)
+  createCanvas(700, 700);
+  s = new Snake();
+  frameRate(10); 
+	food = createVector(random(width), random(height));
+	pickLocation(); 
 }
+
+//function to store snake's location on the grid
+//floor calculates the closest int value that is less than or equal to the value of the parameter.
+function pickLocation() {
+  var cols = floor(width/scl);
+	var rows = floor(height/scl);
+  food = createVector(floor(random(cols)), floor(random(rows)));//this ensure the food is in the grid aligned with snake
+	food.mult(scl);//to expand it back out
+
+
+}
+
 function draw() {
-  background(200,200,200)
-  fill(255)
-  ellipse(ellX,ellY, 40,40)
-  rect(rectX,rectY,60,60)
-  text("hola mundo", 200,200)
-  if(ellX < 0 || ellX > 700){
-   nz = nz * (-1);
-  }
-  if(ellY < 0 || ellY > 600){
-   xz = xz * (-1);
-  }
-  ellX = ellX + nz
-  ellY = ellY + xz
-  text(ellY, 450,200)
-  text(ellX, 250,200)
-  text(rectX, 450,400)
-  text(rectY, 250,400)
+  background(51);
   
-  if(ellX > rectX && ellX < rectX + 60){
-        if(ellY > rectY && ellY < rectY + 60){
-              xz = xz * (-1);
-              nz = nz * (-1);
-        }
-      }
+  //if snake eat food, pick location
+  if (s.eat(food)) {
+  	pickLocation();
+  }
+  s.death();
+  s.update();
+  s.show();
+	
+  //drawing snake food
+  fill(255, 0, 100);
+  rect(food.x, food.y, scl, scl);
+  
 }
-function mouseMoved(){
-  rectX= mouseX-30;
-  rectY= mouseY-30;
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    s.dir(0, -1); //moves 0 along x and -1 (up) along y axis
+  } else if (keyCode === DOWN_ARROW) {
+    s.dir(0, 1);
+  } else if (keyCode === RIGHT_ARROW) {
+    s.dir(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 0);
+  }
 }
+
+
