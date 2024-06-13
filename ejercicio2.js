30ba07aa7650d46750ed7b52fafda1973b9f3d40
mayola <mayola@marketpsychdata.com>
2024-06-12 22:05:22 -0300

    safe commit

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,40 +1,56 @@
 let s;
 let scl = 20;
 let food;
-let flag = 0
-let posxS = 100
-let posyS = 100
-let width = 700
-let height = 700
+let flag = 0;
+let posxS = 100;
+let posyS = 100;
+let width = 700;
+let height = 700;
+let lastMov = 1;
 function setup() {
   createCanvas(width, height);
   s = new Snake();
-  frameRate(10); 
-	//food = createVector(random(width), random(height));
-	//pickLocation(); 
+  frameRate(10);
+  //food = createVector(random(width), random(height));
+  //pickLocation();
 }
 class Snake {
-  Show(){
-    fill(0,255,0)
-    rect(posxS, posyS, scl, scl)
+  Show() {
+    fill(0, 255, 0);
+    rect(posxS, posyS, scl, scl);
   }
-  Dir(x,y){
-        posxS +=x
-        posyS +=y
+  Movement(dir) {
+    switch (dir) {
+      case 0:
+        this.Dir(0, -scl); //arriba
+        break;
+      case 1:
+        this.Dir(0, scl); //abajo
+        break;
+      case 2:
+        this.Dir(-scl, 0); //izquierda
+        break;
+      case 3:
+        this.Dir(scl, 0); //derecha
+        break;
+    }
+  }
+  Dir(x, y) {
+    posxS += x;
+    posyS += y;
   }
 }
 //function to store snake's location on the grid
 //floor calculates the closest int value that is less than or equal to the value of the parameter.
 function pickLocation() {
-  var cols = floor(width/scl);
-	var rows = floor(height/scl);
-  food = createVector(floor(random(cols)), floor(random(rows)));//this ensure the food is in the grid aligned with snake
-	food.mult(scl);//to expand it back out
+  var cols = floor(width / scl);
+  var rows = floor(height / scl);
+  food = createVector(floor(random(cols)), floor(random(rows))); //this ensure the food is in the grid aligned with snake
+  food.mult(scl); //to expand it back out
 }
 
 function draw() {
   background(51);
-
   //if snake eat food, pick location
   //if (s.eat(food)) {
   //	pickLocation();
@@ -45,25 +61,29 @@ function draw() {
   //fill(255, 0, 100);
   //rect(food.x, food.y, scl, scl);
   s.Show();
-}
-
-function keyPressed(keyCode) {
-  let s = new Snake()
-  console.log(keyCode)
-  switch(keyCode.key){
-    case 'ArrowUp':
-    s.Dir(0, -scl); //moves 0 along x and -1 (up) along y axis
-    break
-    case 'ArrowDown':
-    s.Dir(0, scl); //moves 0 along x and -1 (up) along y axis
-    break
-    case 'ArrowLeft':
-    s.Dir(-scl, 0); //moves 0 along x and -1 (up) along y axis
-    break
-    case 'ArrowRight':
-    s.Dir(scl, 0); //moves 0 along x and -1 (up) along y axis
-    break
+  if(posxS < 0 || posxS + scl > width || posyS < 0 || posyS + scl> height){
+    fill(255,0,0)
+    text("GAME OVER", height / 2, width / 2)
+  }else{
+    s.Movement(lastMov)
   }
 }
 
-
+function keyPressed(keyCode) {
+  let s = new Snake();
+  console.log(keyCode);
+  switch (keyCode.key) {
+    case "ArrowUp":
+      lastMov = 0;
+      break;
+    case "ArrowDown":
+      lastMov = 1;
+      break;
+    case "ArrowLeft":
+      lastMov = 2;
+      break;
+    case "ArrowRight":
+      lastMov = 3;
+      break;
+  }
+}
